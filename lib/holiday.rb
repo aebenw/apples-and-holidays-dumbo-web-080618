8bf609ce5473da68bfaa16732e06f0a8feb337fb
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-22 05:04:58 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,13 +56,12 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   str =""
   holiday_hash.each do |season, holiday|
-    str << "#{season.to_s.capitalize!}: " 
+    str << "#{season.to_s.capitalize!}:" 
      holiday.each do |hol, supplies|
        new = hol.to_s.split("_")
        new.each do |word|
         if word == new[-1]
           str << "#{word.capitalize!}: "
-          
         else
           str << "#{word.capitalize!} "
         end
