54d9aac8911ec74c1402e25bc1b6aaee26099583
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-22 04:57:21 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   str =""
   holiday_hash.each do |season, holiday|
-    str << "#{season.to_s.capitalize!}:\n " 
+    str << "#{season.to_s.capitalize!}: " 
      holiday.each do |hol, supplies|
        new = hol.to_s.split("_")
        new.each do |word|
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
       end
       supplies.each do |sup|
         if sup == supplies.last 
-          str << "#{sup}\n"
+          str << "#{sup}"
         else str << "#{sup}, "
         end
       end
