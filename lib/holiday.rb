0409878705deb71e5f32c828d0efba30312a8e4c
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-22 05:32:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,7 +87,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |season, holiday|
-    holiday.map do |day, supply| day.include?("BBQ")
+    holiday.map do |day, supply| day.includes?("BBQ")
     end
   end
 end
